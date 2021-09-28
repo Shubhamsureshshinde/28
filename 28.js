2335a818b3d5b8f811e3c1b02483e4a58ff80273
Shubham Shinde <shindeshubham623@gmail.com>
2021-09-28 12:16:12 +0530

    third time

--- 28.js
+++ 28.js
@@ -1,6 +1,28 @@
-const masschanchal = 89;
+// const masschanchal = 89;
+// const heightchanchal = 1.73;
+// const masspintu= 88;
+// const heightpintu = 1.68;
+
+// const BMIchanchal = masschanchal / heightchanchal**2;
+// const BMIpintu = masspintu / heightpintu**2
+
+// console.log (BMIchanchal, BMIpintu);
+
+
+// if (BMIpintu>BMIchanchal){
+//     console.log("bmi of pintu is greater ")
+// }
+// else{
+//     console.log("bmi of Chanchal is greater ")  
+
+
+
+// GIT ADD the don
+
+
+const masschanchal = 99;
 const heightchanchal = 1.73;
-const masspintu= 88;
+const masspintu= 48;
 const heightpintu = 1.68;
 
 const BMIchanchal = masschanchal / heightchanchal**2;
@@ -13,8 +35,5 @@ if (BMIpintu>BMIchanchal){
     console.log("bmi of pintu is greater ")
 }
 else{
-    console.log("bmi of Chanchal is greater ")  
+    console.log("bmi of Chanchal is greater ")
 }
-
-
-// GIT ADD the don
