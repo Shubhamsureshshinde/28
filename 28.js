65aac648707f6e4850900e2d7b5059ba82e11bea
Shubham Shinde <shindeshubham623@gmail.com>
2021-09-28 11:28:16 +0530

    28 sept

---
+++ 28.js
@@ -0,0 +1,17 @@
+const masschanchal = 89;
+const heightchanchal = 1.73;
+const masspintu= 88;
+const heightpintu = 1.68;
+
+const BMIchanchal = masschanchal / heightchanchal**2;
+const BMIpintu = masspintu / heightpintu**2
+
+console.log (BMIchanchal, BMIpintu);
+
+
+if (BMIpintu>BMIchanchal){
+    console.log("bmi of pintu is greater ")
+}
+else{
+    console.log("bmi of Chanchal is greater ")  
+}
